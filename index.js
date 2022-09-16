ff4eac2fa792f0bd327ec344a1ca220db8ad9fe1
Dhruvi Gandhi <dhruvigandhi25@gmail.com>
2022-09-16 20:58:49 +0530

    translation done

--- index.js
+++ index.js
@@ -22,14 +22,26 @@
 
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input")
+var output = document.querySelector(".output")
 
 function clickEventHandler() {
     console.log("Clicked!!")
     console.log("input", txtInput.value)
+    translate(txtInput.value)
 }
 
 btnTranslate.addEventListener("click", clickEventHandler)
 
+function translate(inputText) {
+    fetch(`https://api.funtranslations.com/translate/valyrian.json?text=${inputText}`)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (json) {
+            console.log(json.contents.translated)
+            output.innerHTML = json.contents.translated
+        })
+}
 
 
 
