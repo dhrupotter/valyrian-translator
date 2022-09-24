6877d53916b080dec6354f25f7eab26a3744e9df
dhrupotter <dhruvigandhi25@gmail.com>
2022-09-24 14:52:14 +0530

    Remove console.logs and minor changes

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ var txtInput = document.querySelector("#txt-input");
 var output = document.querySelector(".output");
 
 function clickEventHandler() {
-  console.log("Clicked!!");
-  console.log(txtInput.value);
   translate(txtInput.value);
 }
 
@@ -40,33 +38,6 @@ function translate(inputText) {
       return response.json();
     })
     .then(function (json) {
-      console.log(json.contents.translated);
       output.innerHTML = json.contents.translated;
     });
 }
-
-// Soham taught code
-
-// const inputElement = document.querySelector('.input');
-// const translateBtn = document.querySelector('.translate-btn');
-// const translatedElement = document.querySelector('.translated');
-
-// function fetchDetails(text) {
-//     // Step 1 -> fetch response from minion translator api by passing my text
-//     fetch(`https://api.funtranslations.com/translate/valyrian.json?text=${text}`)
-//         // Step 2 -> convert into json
-//         .then(function (response) {
-//             return response.json()
-//         })
-//         // Step 3 -> use response to do some task
-//         .then(function (json) {
-//             console.log(json.contents.translated)
-//             translatedElement.innerHTML = `<p>${json.contents.translated}</p>`
-//         })
-// }
-
-// // button -> input element ka text -> fetch Details -> translated
-
-// translateBtn.addEventListener('click', function () {
-//     fetchDetails(inputElement.value)
-// })
